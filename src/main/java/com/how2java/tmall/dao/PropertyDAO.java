5a2c76c065bf7a7f460ba2bc31e6721fd70a78a9
MichaelZhang <z374100121@163.com>
2019-12-06 16:16:19 +0800

    完成属性管理部分功能

---
+++ src/main/java/com/how2java/tmall/dao/PropertyDAO.java
@@ -0,0 +1,13 @@
+package com.how2java.tmall.dao;
+
+import com.how2java.tmall.pojo.Category;
+import com.how2java.tmall.pojo.Property;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+//***********负责与数据库进行交互（dao层/继承了JPA的一系列方法）***********
+
+public interface PropertyDAO extends JpaRepository<Property,Integer> {
+    Page<Property> findByCategory(Category category, Pageable pageable);
+}
