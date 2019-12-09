1a98e9c73e4a2932e551a3b3e9685c004773661d
MichaelZhang <z374100121@163.com>
2019-12-09 11:28:17 +0800

    开始前台页面开发（完成前台首页部分开发）

--- src/main/java/com/how2java/tmall/pojo/Category.java
+++ src/main/java/com/how2java/tmall/pojo/Category.java
@@ -4,6 +4,7 @@ package com.how2java.tmall.pojo;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import javax.persistence.*;
+import java.util.List;
 
 //***********Category的实体类（Bean层）***********
 
@@ -17,6 +18,11 @@ public class Category {
     int id;
 
     String name;
+
+    @Transient
+    List<Product> products;
+    @Transient
+    List<List<Product>> productsByRow;
 
     public int getId() {
         return id;
@@ -32,5 +38,21 @@ public class Category {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public List<List<Product>> getProductsByRow() {
+        return productsByRow;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    public void setProductsByRow(List<List<Product>> productsByRow) {
+        this.productsByRow = productsByRow;
     }
 }
